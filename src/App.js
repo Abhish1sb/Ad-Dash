613293e310984650a055ea6a72012ce024ef0576
Abhish1sb <abhish1albodi@gmail.com>
2022-09-22 16:43:00 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+    <h1>this is the heading of the app page :] </h1>
+    <h1>please add the other components here so that the code is more organised:-</h1>
+    </div>
+  )
+}
+
+export default App;
